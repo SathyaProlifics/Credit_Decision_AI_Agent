5ac8de30307eafc89c10709f8b5c064e4ba9da47
sathya <sathya.filenet@gmail.com>
2026-04-11 16:07:58 -0400

    updated agent file

---
+++ LambdaAPIClient.py
@@ -0,0 +1,7 @@
+# Minimal stub for LambdaAPIClient to prevent import errors
+class LambdaAPIClient:
+    def __init__(self, *args, **kwargs):
+        pass
+
+def should_use_lambda():
+    return False
